cade7878f55dd4ce5ef807f034388aad00bc1221
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-09-02 17:56:31 +0000

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,33 +4,33 @@
  * _atoi - converts a string to an integer
  * @s: pointer to the string
  *
- * Return: integer value of string, or 0 if no numbers
+ * Return: integer value of the string, or 0 if no number found
  */
 int _atoi(char *s)
 {
 	int i = 0;
-	int sign = 1;     /* +1 أو -1 حسب عدد علامات السالب */
-	int started = 0;  /* متى بدأنا نقرأ أرقام */
-	int res = 0;      /* نبني القيمة دائماً كسالب لتفادي مشكلة INT_MIN */
+	int sign = 1;
+	int started = 0;
+	int res = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
-			; /* تجاهل */
+			; /* ignore */
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
-			/* ابنِ القيمة كسالب: res = res*10 - digit */
 			res = res * 10 - (s[i] - '0');
 		}
 		else if (started)
-			break; /* توقف بعد انتهاء كتلة الأرقام */
+			break;
 		i++;
 	}
 
-	/* إن كانت الإشارة موجبة أعد -res، وإلا أعد res كما هو */
-	return (sign > 0) ? -res : res;
+	if (sign > 0)
+		return (-res);
+	return (res);
 }
 
