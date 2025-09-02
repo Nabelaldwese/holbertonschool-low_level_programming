a1e5094512f2b36a698dc02ec10022f369903fe0
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-09-02 17:34:58 +0000

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,30 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
+	unsigned int num = 0;
 	int sign = 1;
-	int result = 0;
+	int i = 0;
 	int started = 0;
 
-	/* مر على كل الأحرف */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
-			; /* تجاهل + */
+			; /* تجاهل */
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
-			result = result * 10 + (s[i] - '0');
+			num = num * 10 + (s[i] - '0');
 		}
 		else if (started)
-			break; /* إذا بدأ الرقم وانتهى نوقف */
+			break;
 		i++;
 	}
 
-	return (result * sign);
+	if (sign == -1)
+		return (-((int)num));
+	else
+		return ((int)num);
 }
 
