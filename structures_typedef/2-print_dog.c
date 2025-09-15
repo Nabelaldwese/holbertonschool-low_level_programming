ebd788480ed20880efd04ef9ff8dd58376d3d760
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-09-15 14:40:48 +0000

    fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,23 +7,23 @@
  * @d: pointer to struct dog
  *
  * Description: If an element of d is NULL, it prints (nil)
- * instead of the element. If d itself is NULL, nothing is printed.
+ * instead of the element. If d is NULL, nothing is printed.
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
+    if (d == NULL)
+        return;
 
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
+    if (d->name == NULL)
+        printf("Name: (nil)\n");
+    else
+        printf("Name: %s\n", d->name);
 
-	printf("Age: %f\n", d->age);
+    printf("Age: %f\n", d->age);
 
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+    if (d->owner == NULL)
+        printf("Owner: (nil)\n");
+    else
+        printf("Owner: %s\n", d->owner);
 }
 
