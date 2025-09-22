6a5f2ce1e3bc756719fa97a097685370ebffc6c6
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-09-22 17:44:34 +0000

    fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,38 +1,39 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_c - print a char
+ * print_char - print a char
  * @ap: va_list
  */
-void print_c(va_list ap)
+static void print_char(va_list ap)
 {
 	printf("%c", (char)va_arg(ap, int));
 }
 
 /**
- * print_i - print an int
+ * print_int - print an int
  * @ap: va_list
  */
-void print_i(va_list ap)
+static void print_int(va_list ap)
 {
 	printf("%d", va_arg(ap, int));
 }
 
 /**
- * print_f - print a float (as double from va_arg)
+ * print_float - print a float (promoted to double)
  * @ap: va_list
  */
-void print_f(va_list ap)
+static void print_float(va_list ap)
 {
 	printf("%f", va_arg(ap, double));
 }
 
 /**
- * print_s - print a string, or (nil) if NULL
+ * print_string - print a string or (nil) if NULL
  * @ap: va_list
  */
-void print_s(va_list ap)
+static void print_string(va_list ap)
 {
 	char *s = va_arg(ap, char *);
 
@@ -45,41 +46,50 @@ void print_s(va_list ap)
 }
 
 /**
- * print_all - prints anything according to a format string
- * @format: "c" char, "i" int, "f" float, "s" char *
+ * print_all - prints anything according to format
+ * @format: list of types: 'c','i','f','s'
  *
- * Description: ignores any other characters in format.
- * Constraints satisfied inside this function:
- * - max 2 while loops (we use exactly 2)
- * - max 2 if statements (we use exactly 1 here; the other check is in helpers)
- * - no for/goto/else/ternary/do...while
+ * Rules satisfied here:
+ * - لا for/goto/do..while
+ * - while مرتين بالضبط
+ * - if مرّة واحدة داخل الدالة
+ * - بدون else
+ * - ≤ 9 متغيّرات
  */
 void print_all(const char * const format, ...)
 {
-	printer_t p[] = {
-		{"c", print_c}, {"i", print_i},
-		{"f", print_f}, {"s", print_s},
-		{NULL, NULL}
+	/* خريطة داخلية لتجنّب الاعتماد على typedef في الهيدر */
+	struct map { char t; void (*f)(va_list); } maps[] = {
+		{'c', print_char}, {'i', print_int},
+		{'f', print_float}, {'s', print_string},
+		{'\0', NULL}
 	};
+
 	va_list ap;
 	unsigned int i = 0, j;
 	char *sep = "";
+	void (*func)(va_list);
 
 	va_start(ap, format);
 
-	while (format && format[i])            /* (1) while over format */
+	while (format && format[i])                /* (1) while */
 	{
 		j = 0;
-		while (p[j].symbol)                /* (2) while over printer table */
+		func = NULL;
+		while (maps[j].t)                      /* (2) while */
 		{
-			if (format[i] == *(p[j].symbol))   /* (1) if in this function */
+			if (format[i] == maps[j].t)        /* (1) if داخل الدالة */
 			{
-				printf("%s", sep);
-				p[j].print(ap);
-				sep = ", ";
+				func = maps[j].f;
 				break;
 			}
 			j++;
+		}
+		if (func)                              /* نفس if السابقة (مو else) */
+		{
+			printf("%s", sep);
+			func(ap);
+			sep = ", ";
 		}
 		i++;
 	}
