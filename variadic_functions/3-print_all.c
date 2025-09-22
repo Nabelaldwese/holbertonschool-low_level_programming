73016e02d5d373da50a350fd9efffeaf24bd9ba8
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-09-22 18:06:23 +0000

    fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * print_char - prints a char
+ * print_char - Prints a char argument
  * @ap: variadic list
  */
 static void print_char(va_list ap)
@@ -12,7 +12,7 @@ static void print_char(va_list ap)
 }
 
 /**
- * print_int - prints an int
+ * print_int - Prints an int argument
  * @ap: variadic list
  */
 static void print_int(va_list ap)
@@ -21,7 +21,7 @@ static void print_int(va_list ap)
 }
 
 /**
- * print_float - prints a float (double in varargs)
+ * print_float - Prints a float/double argument
  * @ap: variadic list
  */
 static void print_float(va_list ap)
@@ -30,10 +30,8 @@ static void print_float(va_list ap)
 }
 
 /**
- * print_string - prints a string or (nil) if NULL
+ * print_string - Prints a string or (nil) when NULL (بدون if/else)
  * @ap: variadic list
- *
- * Note: لا نستخدم if/else هنا. نختار بين مؤشرين بمؤشر فهرسة.
  */
 static void print_string(va_list ap)
 {
@@ -41,28 +39,24 @@ static void print_string(va_list ap)
 	char *nil = "(nil)";
 	char *opt[2];
 
-	opt[0] = nil;      /* عندما s == NULL سنطبع opt[0] */
-	opt[1] = s;        /* عندما s != NULL سنطبع opt[1] */
+	opt[0] = nil;
+	opt[1] = s;
 	printf("%s", opt[s != NULL]);
 }
 
 /**
- * print_all - prints anything according to @format
- * @format: list of types: 'c' 'i' 'f' 's'
+ * print_all - Prints arguments according to @format: c,i,f,s
+ * @format: format string
  *
- * القيود المتحققة هنا:
- * - لا for / goto / do..while / else / ?: .
- * - while = 2 بالضبط.
- * - if = 2 بالضبط داخل هذه الدالة.
- * - <= 9 متغيرات.
+ * Constraints: no for/goto/else/?:/do..while,
+ * exactly 2 while loops and ≤ 2 if in this function, ≤ 9 variables.
  */
 void print_all(const char * const format, ...)
 {
-	/* خريطة محلية لتجنّب الاعتماد على typedef في الهيدر */
-	struct map { char t; void (*f)(va_list); } maps[] = {
-		{'c', print_char}, {'i', print_int},
-		{'f', print_float}, {'s', print_string},
-		{'\0', NULL}
+	/* parallel lookup tables (بدون struct لتجنّب تحذير Betty) */
+	char tags[] = {'c', 'i', 'f', 's', '\0'};
+	void (*funcs[])(va_list) = {
+		print_char, print_int, print_float, print_string, NULL
 	};
 
 	va_list ap;
@@ -71,21 +65,20 @@ void print_all(const char * const format, ...)
 	void (*fn)(va_list);
 
 	va_start(ap, format);
-
-	while (format && format[i])              /* (1) while */
+	while (format && format[i])          /* (1) while */
 	{
 		j = 0;
 		fn = NULL;
-		while (maps[j].t)                    /* (2) while */
+		while (tags[j])                  /* (2) while */
 		{
-			if (format[i] == maps[j].t)      /* (1) if */
+			if (format[i] == tags[j])    /* (1) if */
 			{
-				fn = maps[j].f;
+				fn = funcs[j];
 				break;
 			}
 			j++;
 		}
-		if (fn)                               /* (2) if */
+		if (fn)                          /* (2) if */
 		{
 			printf("%s", sep);
 			fn(ap);
@@ -93,7 +86,6 @@ void print_all(const char * const format, ...)
 		}
 		i++;
 	}
-
 	printf("\n");
 	va_end(ap);
 }
