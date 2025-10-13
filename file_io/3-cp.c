01026568e7a9a19c040af1dbbd7f5cf6ed766b43
Nabelaldwese <Nbelaldwese1@gmail.com>
2025-10-13 15:42:39 +0000

    feat

---
+++ file_io/3-cp.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFSIZE 1024
+
+/**
+ * main - copy the content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success, exits with specific codes on error
+ *
+ * Errors:
+ * 97: wrong argument count
+ * 98: can't read from file_from
+ * 99: can't write to/create file_to
+ * 100: can't close a file descriptor
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[BUFSIZE];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, BUFSIZE)) > 0)
+	{
+		ssize_t written = 0;
+
+		while (written < r)
+		{
+			w = write(fd_to, buffer + written, r - written);
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+				close(fd_from);
+				close(fd_to);
+				exit(99);
+			}
+			written += w;
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
+
